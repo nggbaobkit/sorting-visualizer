8395b11a0392f2ffd497ac7ae153418f4e581f99
Gia Bao <nggbaobkit@gmail.com>
2020-03-30 11:08:30 +0700

    Added array bar value appearance when there are few bars.

--- src/ArrayBar/ArrayBar.jsx
+++ src/ArrayBar/ArrayBar.jsx
@@ -7,6 +7,39 @@ const ArrayBar = props => {
   const array = props.array;
   const numWidth = Math.floor($(document).width() / (array.length * 3));
   const width = `${numWidth}px`;
+  const numMargin =
+    array.length < 5
+      ? 10
+      : array.length < 8
+      ? 8
+      : array.length < 11
+      ? 6
+      : array.length < 20
+      ? 4
+      : array.length < 50
+      ? 3.5
+      : array.length < 100
+      ? 3
+      : array.length < 130
+      ? 2.5
+      : 2;
+  const margin = `${numMargin}px`;
+  const color = numWidth > 20 ? 'white' : 'transparent';
+  const numFont =
+    numWidth > 70
+      ? 23
+      : numWidth > 60
+      ? 18
+      : numWidth > 50
+      ? 16
+      : numWidth > 40
+      ? 14
+      : numWidth > 30
+      ? 12
+      : numWidth > 20
+      ? 10
+      : 8;
+  const fontSize = `${numFont}px`;
 
   return (
     <div className='bar-container'>
@@ -17,8 +50,11 @@ const ArrayBar = props => {
           style={{
             height: `${value}px`,
             width: width,
-            color: 'transparent',
-            backgroundColor: PRIMARY_COLOR
+            backgroundColor: PRIMARY_COLOR,
+            marginLeft: margin,
+            marginRigh: margin,
+            color: color,
+            fontSize: fontSize
           }}
         >
           {value}
